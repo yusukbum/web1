2806ca8a3cba125838a5ad6269a9346d9fd8d0cc
yusukbum <36921890+yusukbum@users.noreply.github.com>
2018-02-28 21:30:44 +0900

    first version

---
+++ 1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>비밀번호를 입력하세요.</p>
+    <form class="" action="3.php" method="post">
+    <input type="password" name="비밀번호">
+    <input type="submit">
+    </form>
+  </body>
+</html>
---
+++ 3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <script type="text/javascript">
+    function a(){
+        var name1 = prompt("나의 이름은 무엇인가요?");
+        if(name1 == '설이'){
+          document.write("정답입니다~짝짝짝");
+        }
+        else{
+          alert("틀렸습니다.........");
+          alert("틀렸습니다.........");
+          alert("틀렸습니다.........");
+          document.write('<img src="ghost.jpg" alt="">');
+        }}
+        a();
+    </script>
+
+  </body>
+</html>
